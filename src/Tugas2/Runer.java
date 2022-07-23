5c0f26409be46c449902527511ea6cc9e48c4f85
Fathur Rahman <fathur.rahman@udana.id>
2022-07-23 18:07:30 +0700

    update

--- src/Tugas2/Runer.java
+++ src/Tugas2/Runer.java
@@ -8,48 +8,70 @@ public class Runer {
 		// TODO Auto-generated method stub
 		Implementer implementer = new Implementer();
 		Scanner myObj = new Scanner(System.in);  
+		
+		System.out.println("Pilih Operation: (+, -, /, *) ");
+	    var operation = myObj.next(); 
 
 		System.out.println("Masukan input pertama: ");
 	    var input1 = myObj.next(); 
 	    System.out.println("Masukan input kedua");
 	    var input2 = myObj.next(); 
-		
-		try {
+	    
+	    switch (operation) {
+		case "+":
+			try {
 
-			implementer.penjumlahan(Integer.parseInt(input1), Integer.parseInt(input2));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Tipe data harus number");  // Output user input
-			e.printStackTrace();
-			throw new Exception("Terjadi error");
+				implementer.penjumlahan(Integer.parseInt(input1), Integer.parseInt(input2));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Tipe data harus number");  // Output user input
+				e.printStackTrace();
+				throw new Exception("Terjadi error");
+			}
+			break;
+		case "-":
+			try {
+				implementer.pengurangan(Integer.parseInt(input1), Integer.parseInt(input2));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Tipe data harus number");  // Output user input
+				e.printStackTrace();
+				throw new Exception("Terjadi error");
+			}
+			break;
+		case "/":
+			try {
+				implementer.pembagian(Integer.parseInt(input1), Integer.parseInt(input2));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Tipe data harus number");  // Output user input
+				e.printStackTrace();
+				throw new Exception("Terjadi error");
+			}
+			break;
+		case "*":
+			try {
+				implementer.perkalian(Integer.parseInt(input1), Integer.parseInt(input2));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				System.out.println("Tipe data harus number");  // Output user input
+				e.printStackTrace();
+				throw new Exception("Terjadi error");
+			}
+			break;
+		default:
+			System.out.println("Operator harus mengandung salah satu dari (+, -, /, *)");  // Output user input
+
+			break;
 		}
 		
-		try {
-			implementer.pengurangan(Integer.parseInt(input1), Integer.parseInt(input2));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Tipe data harus number");  // Output user input
-			e.printStackTrace();
-			throw new Exception("Terjadi error");
-		}
 		
-		try {
-			implementer.perkalian(Integer.parseInt(input1), Integer.parseInt(input2));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Tipe data harus number");  // Output user input
-			e.printStackTrace();
-			throw new Exception("Terjadi error");
-		}
 		
-		try {
-			implementer.pembagian(Integer.parseInt(input1), Integer.parseInt(input2));
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			System.out.println("Tipe data harus number");  // Output user input
-			e.printStackTrace();
-			throw new Exception("Terjadi error");
-		}
+		
+		
+		
+		
+		
 
 	}
 
